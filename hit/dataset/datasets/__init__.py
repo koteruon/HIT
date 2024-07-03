91a382fcf6bdd46354c22498217cbbd737434b56
Chaoen <calvin19961211@gmail.com>
2024-07-04 00:57:05 +0800

    修改回原始的table_tennis的DatasetEngine

--- hit/dataset/datasets/__init__.py
+++ hit/dataset/datasets/__init__.py
@@ -1,8 +1,9 @@
 # from .ava import DatasetEngine
 from .concat_dataset import ConcatDataset
-from .table_tennis_p2anet import DatasetEngine
+from .table_tennis import DatasetEngine
 
-# from .table_tennis import DatasetEngine
+# from .table_tennis_p2anet import DatasetEngine
+
 
 
 # from .jhmdb import DatasetEngine
