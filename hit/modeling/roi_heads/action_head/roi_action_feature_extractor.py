f2325d46ac4fc5f654ed604e5362f1297a89a507
koteruon <calvin19961211@gmail.com>
2025-03-11 23:13:55 +0800

    更改算法增進確定性

--- hit/modeling/roi_heads/action_head/roi_action_feature_extractor.py
+++ hit/modeling/roi_heads/action_head/roi_action_feature_extractor.py
@@ -6,6 +6,7 @@ from hit.modeling import registry
 from hit.modeling.poolers import make_3d_pooler
 from hit.modeling.roi_heads.action_head.hit_structure import make_hit_structure
 from hit.modeling.roi_heads.action_head.pose_transformer import PoseTransformer
+
 # from hit.modeling.roi_heads.action_head.pose_transformerV2 import PoseTransformerV2
 from hit.modeling.utils import cat, pad_sequence, prepare_pooled_feature
 from hit.structures.bounding_box import BoxList
@@ -23,7 +24,10 @@ class MLPFeatureExtractor(nn.Module):
 
         resolution = head_cfg.POOLER_RESOLUTION
 
-        self.max_pooler = nn.MaxPool3d((1, resolution, resolution))
+        # self.max_pooler = nn.MaxPool3d((1, resolution, resolution))
+        target_size = (1, 1, 1)
+        self.max_pooler = nn.AdaptiveMaxPool3d(target_size)
+
         self.pose_out = config.MODEL.HIT_STRUCTURE.DIM_INNER
         if config.MODEL.HIT_STRUCTURE.ACTIVE:
             self.max_feature_len_per_sec = config.MODEL.HIT_STRUCTURE.MAX_PER_SEC
