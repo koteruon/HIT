68da7692588de59e7b5dea9a5297fd1ec87ddf5d
koteruon <calvin19961211@gmail.com>
2025-03-20 13:40:36 +0800

    嘗試看看

--- script_pretrain_skateformer.sh
+++ script_pretrain_skateformer.sh
@@ -3,7 +3,7 @@
 CONFIG_FILE="./config_files/hitnet_pretrain_skateformer.yaml"
 
 i=1
-while [ $i -lt 100 ]
+while [ $i -lt 6 ]
 do
   # 格式化 OUTPUT_DIR 為四位數 (0000 ~ 9999)
   OUTPUT_NUM=$(printf "%02d" $i)
