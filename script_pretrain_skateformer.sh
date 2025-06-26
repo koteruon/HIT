f451bbcb4d51c3d7b34fe72dc5442734e7d16f9a
koteruon <calvin19961211@gmail.com>
2025-06-26 15:23:57 +0800

    執行程式

--- script_pretrain_skateformer.sh
+++ script_pretrain_skateformer.sh
@@ -2,7 +2,7 @@
 
 CONFIG_FILE="./config_files/hitnet_pretrain_skateformer.yaml"
 
-i=25
+i=20
 while [ $i -lt 10000 ]
 do
   # 格式化 OUTPUT_DIR 為四位數 (0000 ~ 9999)
@@ -10,7 +10,7 @@ do
 
   # 修改 OUTPUT_DIR 的最後數字
   # sed -i "s|OUTPUT_DIR: .*|OUTPUT_DIR: \"data/output/hitnet_pose_transformer_with_pretrain_skateformer_20250318_${OUTPUT_NUM}\"|" "$CONFIG_FILE"
-  sed -i "s|OUTPUT_DIR: .*|OUTPUT_DIR: \"data/output/hitnet_pose_transformer_with_pretrain_skateformer_joint_20250526_seed_${OUTPUT_NUM}\"|" "$CONFIG_FILE"
+  sed -i "s|OUTPUT_DIR: .*|OUTPUT_DIR: \"data/output/hitnet_pose_transformer_with_pretrain_skateformer_joint_20250606_seed_${OUTPUT_NUM}\"|" "$CONFIG_FILE"
 
   # 執行 python 指令，seed 直接使用數值格式
   python train_net.py --config-file "$CONFIG_FILE" --seed $i
